038cdc2b61f5538b26e71f396725e0bad847c5d4
Nicolas <nicolaslacerdaa@gmail.com>
2024-07-11 05:15:26 -0300

    bugconsolelog

---
+++ Aula2-variables/index.js
@@ -0,0 +1,15 @@
+// variable = A container that stores a value.
+            //behaves as if  it were the value it contains 
+
+
+// 1. declaration   let x;
+// 2. assignment    x = 100;
+
+let x;
+x = 100;
+
+console.log(x);
+
+let age = 25;
+
+console.log(age);
